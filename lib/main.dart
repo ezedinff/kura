8bb0430698120c7444b821e4a1cd4a64e2f46450
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2020-06-06 16:08:36 +0300

    completed conversation screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kura/pages/ConversationPageList.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
-      home: MyHomePage(title: 'Kura'),
+      home: ConversationPageList()
     );
   }
 }
